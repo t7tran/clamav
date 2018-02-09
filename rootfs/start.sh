766b3a8350f4727b94966a72c045740e6f6e8eb5
Tien Tran <ttran@genixventures.com>
2018-02-09 22:28:57 +1100

    Cleanup and run clamd with gosu

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Bootstrap the database if clamav is running for the first time
-[ -f /data/main.cvd ] || freshclam
+[ -f /clamav/main.cvd ] || freshclam
 
 # Run the update daemon
-freshclam -d -c 6
+gosu clamav freshclam -d -c 6
 
 # Run clamav
-clamd
+exec gosu clamav clamd
