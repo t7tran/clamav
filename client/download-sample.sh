67be0f8cef82f7a4a24156bb9ed74ac962e9468c
Tien Tran <tiendung.tran@gmail.com>
2020-11-01 21:41:50 +1100

    Upgrade to v0.102.4 with additional custom signatures

---
+++ client/download-sample.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+[ ! -f /tmp/eicar.com     ] && wget https://secure.eicar.org/eicar.com     -O /tmp/eicar.com
+[ ! -f /tmp/eicar.com.txt ] && wget https://secure.eicar.org/eicar.com.txt -O /tmp/eicar.com.txt
+[ ! -f /tmp/eicar_com.zip ] && wget https://secure.eicar.org/eicar_com.zip -O /tmp/eicar_com.zip
+[ ! -f /tmp/eicarcom2.zip ] && wget https://secure.eicar.org/eicarcom2.zip -O /tmp/eicarcom2.zip
+
+clamdscan /tmp
--- rootfs/start.sh
+++ rootfs/start.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-chown clamav:clamav /clamav
-
 # Bootstrap the database if clamav is running for the first time
 [ -f /clamav/main.cvd ] || freshclam
 
 # Run the update daemon
-gosu clamav freshclam -d -c 6
+freshclam -d -c 6
 
-sed -i "s/^StreamMaxLength 10M$/StreamMaxLength ${STREAM_MAX_LENGTH:-10M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxScanSize 150M$/MaxScanSize ${MAX_SCAN_SIZE:-150M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxFileSize 30M$/MaxFileSize ${MAX_FILE_SIZE:-30M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxRecursion 10$/MaxRecursion ${MAX_RECURSION:-10}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxFiles 15000$/MaxFiles ${MAX_FILES:-15000}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxEmbeddedPE 10M$/MaxEmbeddedPE ${MAX_EMBEDDED_PE:-10M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxHTMLNormalize 10M$/MaxHTMLNormalize ${MAX_HTML_NORMALIZE:-10M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxHTMLNoTags 2M$/MaxHTMLNoTags ${MAX_HTML_NO_TAGS:-2M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxScriptNormalize 5M$/MaxScriptNormalize ${MAX_SCRIPT_NORMALIZE:-5M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxZipTypeRcg 1M$/MaxZipTypeRcg ${MAX_ZIP_TYPE_RCG:-1M}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxPartitions 128$/MaxPartitions ${MAX_PARTITIONS:-128}/g" /etc/clamav/clamd.conf
-sed -i "s/^MaxIconsPE 200$/MaxIconsPE ${MAX_ICONS_PE:-200}/g" /etc/clamav/clamd.conf
-sed -i "s/^PCREMatchLimit 10000$/PCREMatchLimit ${PCRE_MATCH_LIMIT:-10000}/g" /etc/clamav/clamd.conf
-sed -i "s/^PCRERecMatchLimit 10000$/PCRERecMatchLimit ${PCRE_REC_MATCH_LIMIT:-10000}/g" /etc/clamav/clamd.conf
+conf=/etc/clamav/clamd.conf
+
+if touch $conf &>/dev/null; then 
+  sed -i "s/^StreamMaxLength 10M$/StreamMaxLength ${STREAM_MAX_LENGTH:-10M}/g" $conf
+  sed -i "s/^MaxScanSize 150M$/MaxScanSize ${MAX_SCAN_SIZE:-150M}/g" $conf
+  sed -i "s/^MaxFileSize 30M$/MaxFileSize ${MAX_FILE_SIZE:-30M}/g" $conf
+  sed -i "s/^MaxRecursion 10$/MaxRecursion ${MAX_RECURSION:-10}/g" $conf
+  sed -i "s/^MaxFiles 15000$/MaxFiles ${MAX_FILES:-15000}/g" $conf
+  sed -i "s/^MaxEmbeddedPE 10M$/MaxEmbeddedPE ${MAX_EMBEDDED_PE:-10M}/g" $conf
+  sed -i "s/^MaxHTMLNormalize 10M$/MaxHTMLNormalize ${MAX_HTML_NORMALIZE:-10M}/g" $conf
+  sed -i "s/^MaxHTMLNoTags 2M$/MaxHTMLNoTags ${MAX_HTML_NO_TAGS:-2M}/g" $conf
+  sed -i "s/^MaxScriptNormalize 5M$/MaxScriptNormalize ${MAX_SCRIPT_NORMALIZE:-5M}/g" $conf
+  sed -i "s/^MaxZipTypeRcg 1M$/MaxZipTypeRcg ${MAX_ZIP_TYPE_RCG:-1M}/g" $conf
+  sed -i "s/^MaxPartitions 128$/MaxPartitions ${MAX_PARTITIONS:-128}/g" $conf
+  sed -i "s/^MaxIconsPE 200$/MaxIconsPE ${MAX_ICONS_PE:-200}/g" $conf
+  sed -i "s/^PCREMatchLimit 10000$/PCREMatchLimit ${PCRE_MATCH_LIMIT:-10000}/g" $conf
+  sed -i "s/^PCRERecMatchLimit 10000$/PCRERecMatchLimit ${PCRE_REC_MATCH_LIMIT:-10000}/g" $conf
+fi
 
 # Run clamav
-exec gosu clamav clamd
+exec clamd
