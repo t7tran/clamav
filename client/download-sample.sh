2f0f8a657dd4dfe4715cb12ce8415ae06d35c1cb
Tien Tran <tien@transfamily.info>
2023-05-05 13:17:34 +1000

    Add clamdscan2 helper

--- client/download-sample.sh
+++ client/download-sample.sh
@@ -5,4 +5,4 @@
 [ ! -f /tmp/eicar_com.zip ] && wget https://secure.eicar.org/eicar_com.zip -O /tmp/eicar_com.zip
 [ ! -f /tmp/eicarcom2.zip ] && wget https://secure.eicar.org/eicarcom2.zip -O /tmp/eicarcom2.zip
 
-clamdscan /tmp
+clamdscan2 /tmp
